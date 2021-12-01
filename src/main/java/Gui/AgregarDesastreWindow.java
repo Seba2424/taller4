71706e0384783a3148f3958b1deea5e5581e3139
Sebastian <s.calfunao01@ufromail.cl>
2021-12-01 10:17:50 -0300

    COMMIT 10:17AM

--- src/main/java/Gui/AgregarDesastreWindow.java
+++ src/main/java/Gui/AgregarDesastreWindow.java
@@ -5,9 +5,9 @@ public class AgregarDesastreWindow {
 	private TextInputPanel _cantidadInput;
 	private TextInputPanel _topeCantidadPAInput;
 	private TextInputPanel _topeCantidadIEInput;
-	private TextInputPanel _añoTopeDCInput;
-	private TextInputPanel _añoTopeDPAInput;
-	private TextInputPanel _añoTopeDIEInput;
+	private TextInputPanel _aï¿½oTopeDCInput;
+	private TextInputPanel _aï¿½oTopeDPAInput;
+	private TextInputPanel _aï¿½oTopeDIEInput;
 	private TextInputPanel _previousFrame;
 	public TextInputPanel _unnamed_TextInputPanel_;
 	public DesastresWindow _unnamed_DesastresWindow_;
